1bf042acecf94cc9837b98cf609b1d3ea82d7928
Julian Gaitan <julian89net@gmail.com>
2023-12-06 21:35:45 -0500

    problem 4

--- problem-4-largest-palindrome-product.js
+++ problem-4-largest-palindrome-product.js
@@ -2,7 +2,7 @@ function largestPalindromeProduct(n) {
 
   let limit = Math.pow(10, n) - 1;
   let largest = 0;
-  for (let x = limit; x > limit/2; x--) {
+  for (let x = limit; x > (3*limit/4); x--) {
     for (let y = limit; y >= x; y--) {
       let z = x * y;
       if (isPalindromicNumber(z) && z > largest) {
@@ -14,15 +14,16 @@ function largestPalindromeProduct(n) {
 }
 
 function isPalindromicNumber(number) {
-  let str = number + "";
-  if (str.length < 2) return false;
-  while (str.length >= 2) {
-    if (str.charAt(0) != str.charAt(str.length - 1)) {
-      return false;
-    }
-    str = str.substring(1, str.length - 1);
+  return number == reverseNumber(number);
+}
+
+function reverseNumber(number) {
+  let revNumber = number % 10;
+  while (number >= 10) {
+	number = Math.floor(number / 10);
+    revNumber = (revNumber) * 10 + (number % 10);
   }
-  return true;
+  return revNumber;
 }
 
 console.log(largestPalindromeProduct(3));
