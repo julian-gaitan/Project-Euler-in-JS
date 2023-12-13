2ae15d557732d8e79a67dd63718c244e4ed0bf2d
Julian Gaitan <julian89net@gmail.com>
2023-12-12 20:52:31 -0500

    problem 14

---
+++ problem-14-longest-collatz-sequence.js
@@ -0,0 +1,31 @@
+
+function longestCollatzSequence(limit) {
+
+  let longest = 0, starting;
+  for (let i = 3; i <= limit; i++) {
+    let n = i, sequence = [n];
+    while ((n = nextSequenceNumber(n)) != 1) {
+      sequence.push(n);
+    }
+    sequence.push(1);
+    // console.log(sequence);
+    if (sequence.length > longest) {
+      longest = sequence.length;
+      starting = i;
+      // console.log(longest);
+    }
+  }
+  return starting;
+}
+
+function nextSequenceNumber(n) {
+  if (n % 2 == 0) {
+    n /= 2;
+  } else {
+    n *= 3;
+    n++;
+  }
+  return n;
+}
+
+console.log(longestCollatzSequence(46500));
