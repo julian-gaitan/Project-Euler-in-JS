189e8c4e42baad9a504dc476fb978e3a416c6249
Julian Gaitan <julian89net@gmail.com>
2023-12-12 19:10:28 -0500

    problem 12

--- problem-12-highly-divisible-triangular-number.js
+++ problem-12-highly-divisible-triangular-number.js
@@ -2,24 +2,27 @@
 function divisibleTriangleNumber(n) {
 
   let sequence = 1, count = 1;
-  while (n > findDivisors_f(sequence).length) {
+  while (n > findDivisors(sequence).length) {
     sequence += ++count;
   }
   return sequence;
 }
 
-function findDivisors_f(n) {
+function findDivisors(n) {
   let lowerDiv = [1];
   let upperDiv = [n];
-  for (let i = 2; i < upperDiv[upperDiv.length-1]; i++) {
+  let limit = Math.sqrt(n);
+  for (let i = 2; i <= limit; i++) {
     if ((n % i) == 0) {
       lowerDiv.push(i);
-      if (n/i > i) {
+      if (i < limit) {
         upperDiv.push(n/i);
       }
     }
   }
   let divisors = [...lowerDiv, ...upperDiv.reverse()];
+  // console.log(n);
+  // console.log(divisors);
   return divisors;
 }
 
