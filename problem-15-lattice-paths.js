623a508020c9f9db61acb5bc1d78f535f3d5488a
Julian Gaitan <julian89net@gmail.com>
2023-12-14 14:16:10 -0500

    problem 15

--- problem-15-lattice-paths.js
+++ problem-15-lattice-paths.js
@@ -1,74 +1,13 @@
 
-let paths = [
-    [{x:1,y:0},{x:2,y:0},{x:2,y:1},{x:2,y:2}],
-    [{x:1,y:0},{x:1,y:1},{x:1,y:2},{x:2,y:2}],
-    [{x:1,y:0},{x:1,y:1},{x:2,y:1},{x:2,y:2}],
-    [{x:0,y:1},{x:1,y:1},{x:1,y:2},{x:2,y:2}],
-    [{x:0,y:1},{x:1,y:1},{x:2,y:1},{x:2,y:2}],
-    [{x:0,y:1},{x:0,y:2},{x:1,y:2},{x:2,y:2}]
-  ];
-
-function newPoint(x, y) {
-    return {
-        x: x,
-        y: y
-    };
-}
-
 function latticePaths(gridSize) {
     
-    if (paths[0][paths[0].length-1].x < gridSize) {
-        let size = paths[0][paths[0].length-1].x;
-        let newPaths = [];
-        for (const path of paths) {
-            // let newPath = [];
-            for (let point of path) {
-                // newPath.push(point);
-                if (point.x == size && point.y < size) {
-                    let anotherPath = [point];
-                    point = newPoint(point.x+1, point.y);
-                    anotherPath.push(point);
-                    while (point.y <= size) {
-                        point = newPoint(point.x, point.y+1);
-                        anotherPath.push(point);
-                    }
-                    newPaths.push(anotherPath);
-                } else if (point.x < size && point.y == size) {
-                    let anotherPath = [point];
-                    point = newPoint(point.x, point.y+1);
-                    anotherPath.push(point);
-                    while (point.x <= size) {
-                        point = newPoint(point.x+1, point.y);
-                        anotherPath.push(point);
-                    }
-                    newPaths.push(anotherPath);
-                } else if (point.x == size && point.y == size) {
-                    let anotherPathA = [point];
-                    let anotherPathB = [point];
-
-                    anotherPathA.push(newPoint(point.x+1, point.y));
-                    anotherPathA.push(newPoint(point.x+1, point.y+1));
-                    newPaths.push(anotherPathA);
-                    anotherPathB.push(newPoint(point.x, point.y+1));
-                    anotherPathB.push(newPoint(point.x+1, point.y+1));                    
-                    newPaths.push(anotherPathB);
-                }
-            }
+    let paths = Array(gridSize).fill(1);
+    for (let order = 2; order <= gridSize; order++) {
+        for (let i = 1; i < gridSize; i++) {
+            paths[i] += paths[i-1];
         }
-        paths = newPaths;
-        return latticePaths(gridSize);
-    } else {
-        return paths.length;
     }
+    return paths.reduce((pre, cur) => pre + cur, 0) * 2;
 }
-  
-// latticePaths(9).forEach(arr => {
-//     let line = "";
-//     arr.forEach(obj => {
-//         line += JSON.stringify(obj) + ", ";
-//     });
-//     console.log(line);
-// });
-// console.log(paths.length);
-console.log(latticePaths(9));
-  
+
+console.log(latticePaths(20));
