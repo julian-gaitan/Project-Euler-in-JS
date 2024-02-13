7eb78892f7c279d466e92c52a886ffef967aa703
Julian Gaitan <julian89net@gmail.com>
2024-02-13 09:23:06 -0500

    problem 28

---
+++ problem-28-number-spiral-diagonals.js
@@ -0,0 +1,19 @@
+
+function spiralDiagonals(n) {
+
+    if (n % 2 == 0 || n < 3) {
+        return -1;
+    }
+    n = (n-1)/2;
+    let increment = 2, lastNum = 1, sum = 1;;
+    for (let i = 1; i <= n; i++) {
+        for (let j = 0; j < 4; j++) {
+            lastNum += increment;
+            sum += lastNum;
+        }
+        increment += 2;
+    }
+    return sum;
+}
+
+console.log(spiralDiagonals(1001));
